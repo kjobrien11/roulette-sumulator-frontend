6b3c6d9947642969e1feef10cbb156f13f924565
KJ OBrien <kjokjo219@gmail.com>
2025-08-14 18:42:43 -0400

    updates odd even and red black green

--- roulette-sumulator-angular/src/app/stats/stats.component.ts
+++ roulette-sumulator-angular/src/app/stats/stats.component.ts
@@ -13,6 +13,20 @@ export class StatsComponent implements OnInit{
 
   ngOnInit(){
     console.log("HI",this.data)
+
+  }
+
+  formatOddEven(): string {
+    const odd = (this.data.odd / this.data.total_spins) * 100;
+    const even = (this.data.even / this.data.total_spins) * 100;
+    return `${odd.toFixed(2)}%:${even.toFixed(2)}%`;
+  }
+
+  formatRedBlackGreen(): string {
+    const red = (this.data.reds / this.data.total_spins) * 100;
+    const black = (this.data.blacks / this.data.total_spins) * 100;
+    const green = (this.data.greens / this.data.total_spins) * 100;
+    return `${red.toFixed(2)}%:${black.toFixed(2)}%:${green.toFixed(2)}%`;
   }
 
 }
