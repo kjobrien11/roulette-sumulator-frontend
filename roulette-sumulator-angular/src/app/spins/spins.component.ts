b98fd46b7671ef6a16ec57338e99ec92b50913ea
KJ OBrien <kjokjo219@gmail.com>
2025-08-16 13:29:57 -0400

    dynamic render ball list

--- roulette-sumulator-angular/src/app/spins/spins.component.ts
+++ roulette-sumulator-angular/src/app/spins/spins.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 import { RecentSpinComponent } from '../recent-spin/recent-spin.component';
+import { NgFor, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-spins',
-  imports: [RecentSpinComponent],
+  imports: [RecentSpinComponent, NgFor, NgClass],
   templateUrl: './spins.component.html',
   styleUrl: './spins.component.css'
 })
 export class SpinsComponent {
 
+  MAX_LIST_SIZE = 5;
+
   spin_sample_data: any = [
-    {
-      "value": "33",
-      "color": "black",
-      "parity": "odd"
-    },
+
     {
       "value": "29",
       "color": "black",
@@ -31,10 +30,27 @@ export class SpinsComponent {
       "parity": "odd"
     },
     {
+      "value": "33",
+      "color": "black",
+      "parity": "odd"
+    },
+    {
       "value": "00",
       "color": "green",
       "parity": "neither"
     }
   ]
+
+  spin(){
+    if(this.spin_sample_data.length +1 > this.MAX_LIST_SIZE){
+      this.spin_sample_data.pop();
+    }
+    this.spin_sample_data.unshift({
+      "value": "0",
+      "color": "green",
+      "parity": "neither"
+    })
+
+  }
 
 }
