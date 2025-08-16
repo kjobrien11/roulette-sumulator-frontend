7292b0acd2cb2e95fd3b97a7e595426b9a31a311
KJ OBrien <kjokjo219@gmail.com>
2025-08-16 17:26:31 -0400

    adds wheel that spins when clicked

--- roulette-sumulator-angular/src/app/spinner/spinner.component.ts
+++ roulette-sumulator-angular/src/app/spinner/spinner.component.ts
@@ -1,11 +1,25 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-spinner',
-  imports: [],
+  standalone: true,
   templateUrl: './spinner.component.html',
-  styleUrl: './spinner.component.css'
+  styleUrl: './spinner.component.css',
+  imports: [NgClass]
 })
 export class SpinnerComponent {
+  isSpinning = false;
 
+  startSpin() {
+    this.isSpinning = false;
+    setTimeout(() => {
+      this.isSpinning = true;
+    });
+  }
+
+  stopSpin() {
+    this.isSpinning = false;
+  }
 }
